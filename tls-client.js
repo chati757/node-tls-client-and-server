87893cc469039742844918ab13824777c7b05ff9
chati757 <i-fennekon@hotmail.com>
2018-06-23 23:53:34 +0700

    update end and close socket server and add meaning of stdin pipe and resume

--- tls-client.js
+++ tls-client.js
@@ -9,25 +9,28 @@ const options = {
     port: 3000,
     rejectUnauthorized:true,
     requestCert:true
-};
+}
 
 const socket = tls.connect(options, () => {
     console.log('client connected', 
         socket.authorized ? 'authorized' : 'unauthorized')
-    process.stdin.pipe(socket)
-    process.stdin.resume()
-});
+    process.stdin.pipe(socket)//point to socket
+    process.stdin.resume()//Begin reading from stdin so the process does not exit.
+})
 
 socket.setEncoding('utf8')
 
 socket.on('data', (data) => {
+    console.log('datastage')
     console.log(data)
-});
+})
 
 socket.on('error', (error) => {
+    console.log('errorstage')
     console.log(error)
-});
+})
 
 socket.on('end', (data) => {
+    console.log('endstage')
     console.log('Socket end event')
-});
+})
--- tls-server.js
+++ tls-server.js
@@ -7,7 +7,7 @@ const options = {
     ca: fs.readFileSync('./tls-cer/ca-crt.pem'), 
     requestCert: true, 
     rejectUnauthorized: true
-}; 
+}
 
 const server = tls.createServer(options, (socket) => {
     console.log('server connected', 
@@ -15,13 +15,24 @@ const server = tls.createServer(options, (socket) => {
     
     socket.on('error', (error) => {
         console.log(error)
-    });
+    })
     
     socket.write('welcome!\n')
     socket.setEncoding('utf8')
-    socket.pipe(process.stdout)
-    socket.pipe(socket)
-});
+    socket.on('data', (data) => {
+        console.log('datastage')
+        console.log(data)
+        console.log('afterdatastage')
+        if(data=="end\r\n"){
+            socket.end('goodbye\n')
+            server.close()
+        }
+    }).on('error', (err) => {
+        // handle errors here
+        console.log('handle errors here')
+        throw err;
+      })
+})
 
 server.listen(3000, () => {
     console.log('server listening port 3000')
